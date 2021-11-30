9f9adc59f79718daa4a1beec2a3fa65cae1cc2d8
Remco <remcotaal@hotmail.com>
2021-11-30 16:58:22 +0100

    Add new exceptions for feedback and hint

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/exception/InvalidFeedbackException.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/exception/InvalidFeedbackException.java
@@ -2,6 +2,7 @@ package nl.hu.cisq1.lingo.trainer.domain.exception;
 
 import nl.hu.cisq1.lingo.trainer.domain.LetterFeedback;
 
+import java.text.MessageFormat;
 import java.util.List;
 
 public class InvalidFeedbackException extends RuntimeException {
@@ -10,6 +11,6 @@ public class InvalidFeedbackException extends RuntimeException {
     }
 
     public static InvalidFeedbackException wrongLength(String attempt, List<LetterFeedback> letterFeedbackList) {
-        return new InvalidFeedbackException(String.format("attempt length: {} is not the same as letterFeedbackList length: {}", attempt.length(), letterFeedbackList.size()));
+        return new InvalidFeedbackException(MessageFormat.format("attempt length: {0} is not the same as letterFeedbackList length: {1}", attempt.length(), letterFeedbackList.size()));
     }
 }
---
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/exception/InvalidHintException.java
@@ -0,0 +1,24 @@
+package nl.hu.cisq1.lingo.trainer.domain.exception;
+
+import nl.hu.cisq1.lingo.trainer.domain.LetterFeedback;
+
+import java.text.MessageFormat;
+import java.util.List;
+
+public class InvalidHintException extends RuntimeException {
+    public InvalidHintException(String message) {
+        super(message);
+    }
+
+    public static InvalidHintException wrongPreviousHintAndWordToGuessLength(List<Character> previousHint, String wordToGuess) {
+        return new InvalidHintException(MessageFormat.format("previous hint length: {0} is not the same as wordToGuess length: {1}", previousHint.size(), wordToGuess.length()));
+    }
+
+    public static InvalidHintException wrongLetterFeedbackListAndWordToGuessLength(List<LetterFeedback> letterFeedbackList, String wordToGuess) {
+        return new InvalidHintException(MessageFormat.format("letterfeedback list length: {0} is not the same as wordToGuess length: {1}", letterFeedbackList.size(), wordToGuess.length()));
+    }
+
+    public static InvalidHintException wrongPreviousHintAndLetterFeedbackListLength(List<Character> previousHint, List<LetterFeedback> letterFeedbackList) {
+        return new InvalidHintException(MessageFormat.format("previous hint length: {0} is not the same as letterfeedback list length: {1}", previousHint.size(), letterFeedbackList.size()));
+    }
+}
